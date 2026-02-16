56c23a4cb9d47a82e9361ffab9096bb0ce3b611c
Camellia <134266374+JssXmy@users.noreply.github.com>
2026-02-16 10:15:49 +0800

    Update build_graph_dataset.py

--- build_graph_dataset.py
+++ build_graph_dataset.py
@@ -1,9 +1,9 @@
 # generate dataset
 from utils import build_dataset
 args={}
-args['input_csv'] = 'data/AquaTox_scr.csv'
-args['output_bin'] = 'data/AquaTox_scr.bin'
-args['output_csv'] = 'data/AquaTox_scr_group.csv'
+args['input_csv'] = 'data/AquaTox.csv'
+args['output_bin'] = 'data/AquaTox.bin'
+args['output_csv'] = 'data/AquaTox_group.csv'
 
 build_dataset.built_data_and_save_for_splited(
         origin_path=args['input_csv'],
